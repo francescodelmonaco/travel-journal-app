910acaf3c2f7b8e262a9b3fcaa94a50ea5d9ef24
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-07-29 14:27:29 +0200

    add: home page and routing with react router dom, install tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </BrowserRouter>
   )
 }
