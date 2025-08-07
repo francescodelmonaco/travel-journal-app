81d76fafd198eec24306f2c73460e757d6fb0d71
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-08-07 16:28:11 +0200

    add: post card component

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function PostCard({ pageId, event, location, date, image }) {
+    return (
+        <Link
+            to={`/${pageId}`}
+            className="flex flex-col gap-2"
+        >
+            <img
+                src={image || "https://hips.hearstapps.com/hmg-prod/images/logan-armstrong-hvhfqhdyciu-unsplash-1-1606122043.jpg?crop=0.66640625xw:1xh;center,top&resize=640:*"}
+                alt={`Foto ${event}`}
+                className="rounded relative"
+            />
+            <h3 className="font-bold text-xl">{event}</h3>
+            <p>📍 {location}</p>
+            <p className="absolute self-end mt-2 me-2 bg-white py-1.5 px-3 rounded-full shadow shadow-gray-500">{date}</p>
+        </Link>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
 import { useGlobalContext } from "../contexts/GlobalContext"
 
 // components
 import Form from "../components/Form";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
     const { posts, isOpen, setIsOpen } = useGlobalContext();
@@ -38,19 +38,13 @@ export default function Home() {
 
                         return (
                             <div key={index} className="bg-white rounded p-3 shadow shadow-gray-500">
-                                <Link
-                                    to={`/${id}`}
-                                    className="flex flex-col gap-2"
-                                >
-                                    <img
-                                        src={image || "https://hips.hearstapps.com/hmg-prod/images/logan-armstrong-hvhfqhdyciu-unsplash-1-1606122043.jpg?crop=0.66640625xw:1xh;center,top&resize=640:*"}
-                                        alt={`Foto ${event}`}
-                                        className="rounded relative"
-                                    />
-                                    <h3 className="font-bold text-xl">{event}</h3>
-                                    <p>📍 {location}</p>
-                                    <p className="absolute self-end mt-2 me-2 bg-white py-1.5 px-3 rounded-full shadow shadow-gray-500">{newDate}</p>
-                                </Link>
+                                <PostCard
+                                    pageId={id}
+                                    event={event}
+                                    location={location}
+                                    date={newDate}
+                                    image={image}
+                                />
                             </div>
                         )
                     })
