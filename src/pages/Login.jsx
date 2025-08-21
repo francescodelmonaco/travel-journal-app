0acfa88102ec7a2d6d8f4c2813862620cc5c37a1
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-08-21 20:00:47 +0200

    fix: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,21 +47,31 @@ export default function Login() {
 
     if (!session) {
         return (
-            <div className="w-4/5 mx-auto flex flex-col">
-                <div className="w-full mx-auto flex justify-center py-15">
+            <div className="w-4/5 mx-auto flex flex-col justify-center gap-10 text-center min-h-screen">
+                <span
+                    className='text-(--street) text-2xl'
+                >Benvenuto su...</span>
+
+                <div className="w-full sm:w-1/2 self-center flex justify-center bg-(--yellow) py-5 -rotate-3 shadow-xl">
                     <img
                         className="w-20"
                         src={Logo}
                         alt="Travel Journal Logo"
                     />
 
-                    <h1 className="text-3xl text-center font-bold self-center text-(--street)">Travel Journal</h1>
+                    <h1 className="text-3xl text-center font-bold self-center text-(--street) text-shadow-xs ">Travel Journal</h1>
                 </div>
 
+                <p className='text-(--street) text-2xl'>
+                    Un diario di viaggio interattivo per raccogliere e rivivere i momenti più significativi delle tue vacanze estive.
+                </p>
+
+                <i className="fa-regular fa-circle-down text-4xl text-(--street) self-center animate-bounce"></i>
+
                 <button
-                    className="cursor-pointer bg-(--white) border border-(--street) p-4 rounded-3xl font-bold text-(--street) flex align-middle justify-center gap-3 w-1/2 self-center"
+                    className="cursor-pointer bg-(--white) border border-(--street) p-4 rounded-3xl font-bold text-(--street) flex align-middle justify-center gap-3 w-full md:w-1/3 self-center shadow"
                     onClick={signInWithGoogle}>
-                    <i class="fa-brands fa-google self-center"></i>Accedi con Google
+                    <i className="fa-brands fa-google self-center"></i>Accedi con Google
                 </button>
             </div>
         )
